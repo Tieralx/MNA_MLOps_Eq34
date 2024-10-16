f2b05a9bad697cf5ebbb41f3ae85a2e3be18416a
Alejandro Emmanuel Silva Ortega <a01794545@tec.mx>
2024-10-16 11:12:18 -0600

    renamed: renamed files to cleanup

--- src/mlflow_integation_yaml.py
+++ src/mlflow_integation_yaml.py
@@ -5,33 +5,32 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 from sklearn.pipeline import Pipeline
 from sklearn.preprocessing import StandardScaler
-from sklearn.model_selection import train_test_split, cross_val_score
+from sklearn.model_selection import train_test_split, GridSearchCV
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import confusion_matrix, classification_report, accuracy_score
 from sklearn.decomposition import PCA
 from mlflow.models import infer_signature
 import yaml
 
-#yaml file load
+# Cargar archivo YAML con configuración
 with open("MNA_MLOps_Eq34\src\params.yaml") as config_file:
     config_params = yaml.safe_load(config_file)
 
-# Clase del Modelo
+# Clase del Modelo con Grid Search
 class CervicalCancerModel:
-    def __init__(self, filepath, target= config_params["data"]["target_column"]):
+    def __init__(self, filepath, target=config_params["data"]["target_column"]):
         self.filepath = filepath
         self.target = target
         self.model_pipeline = Pipeline([
             ('scaler', StandardScaler()),
-            ('pca', PCA(n_components=config_params["preprocessing"]["PCA_threshold"])),  # Aplicar PCA para reducir dimensiones, basado en un umbral del 90%
-            ('classifier', LogisticRegression(solver=config_params["train"]["solver"], multi_class=config_params["train"]["multi_class"], random_state=config_params["train"]["random_state"], max_iter=config_params["train"]["max_iter"] ))
+            ('pca', PCA(n_components=config_params["preprocessing"]["PCA_threshold"])),  # Aplicar PCA
+            ('classifier', LogisticRegression())
         ])
         self.X_train, self.X_test, self.y_train, self.y_test = [None] * 4
 
     def load_data(self):
         # Cargar los datos
         self.data = pd.read_csv(self.filepath)
-             
         mlflow.log_input(mlflow.data.from_pandas(self.data), context="raw_data")
         return self
 
@@ -45,102 +44,106 @@ class CervicalCancerModel:
         upper_bound = Q3 + 1.5 * IQR
         outliers = ((self.data[numeric_columns] < lower_bound) | (self.data[numeric_columns] > upper_bound))
         self.data = self.data[~outliers.any(axis=1)]
-        
+
         skew = self.data.skew()
         log_transform_columns = [index for index, value in skew.items() if value <= -1]
         sqrt_transform_columns = [index for index, value in skew.items() if -1 < value < -0.5]
-        
+
         self.data[log_transform_columns] = self.data[log_transform_columns].apply(np.log1p)
         self.data[sqrt_transform_columns] = self.data[sqrt_transform_columns].apply(np.sqrt)
-        
-        
+
         mlflow.log_input(mlflow.data.from_pandas(self.data), context="preprocessed_data")
         # Dividir los datos en características (X) y objetivo (y)
         X = self.data.drop(self.target, axis=1)
         y = self.data[self.target]
-        
+
         # Dividir el dataset en conjuntos de entrenamiento y prueba
-        self.X_train, self.X_test, self.y_train, self.y_test = train_test_split(X, y, test_size=config_params["data_split"]["test_size"], random_state=config_params["data_split"]["random_state"], shuffle=config_params["data_split"]["shuffle"])
+        self.X_train, self.X_test, self.y_train, self.y_test = train_test_split(
+            X, y, test_size=config_params["data_split"]["test_size"], random_state=config_params["data_split"]["random_state"],
+            shuffle=config_params["data_split"]["shuffle"]
+        )
         return self
 
-    def train_model(self):
+    def train_model_with_grid_search(self):
+        # Configurar Grid Search con los parámetros del YAML
+        param_grid = config_params["grid_search"]["param_grid"]
+        grid_search = GridSearchCV(self.model_pipeline, param_grid, cv=config_params["grid_search"]["cv"], scoring=config_params["grid_search"]["scoring"])
 
-        
-        
-            # Registrar parámetros del modelo
-            mlflow.log_param("scaler", "StandardScaler")
-            mlflow.log_param("pca_n_components", config_params["preprocessing"]["PCA_threshold"])
-            mlflow.log_param("classifier", "LogisticRegression")
-            mlflow.log_param("model_solver" ,config_params["train"]["solver"])
-            mlflow.log_param("model_multi_class", config_params["train"]["multi_class"]) 
-            mlflow.log_param("model_random_state",config_params["train"]["random_state"])
-            mlflow.log_param("model_max_iter", config_params["train"]["max_iter"])
-            
-            # Entrenar el modelo
-            self.model_pipeline.fit(self.X_train, self.y_train)
-            
-            # Imprimir los componentes principales seleccionados por PCA
-            pca = self.model_pipeline.named_steps['pca']
-            num_components = pca.n_components_
-            mlflow.log_param("n_components", num_components)
-            print(f"Número total de componentes seleccionados por PCA: {num_components}")
-            
-            # Predecir los valores del conjunto de prueba
-            y_pred = self.model_pipeline.predict(self.X_test)
-            signature = infer_signature(self.X_test, y_pred)
-            
-            # Registrar accuracy
-            accuracy = accuracy_score(self.y_test, y_pred)
-            mlflow.log_metric("accuracy", accuracy)
-            print(f"Accuracy: {accuracy}")
-            
-            # Matriz de confusión
-            cm = confusion_matrix(self.y_test, y_pred)
-            cm_df = pd.DataFrame(cm, index=config_params["reports"]["target_names"] , columns=config_params["reports"]["target_names"])
-            print("Matriz de Confusión:")
-            print(cm_df)
-            # Guardar la matriz de confusión como una imagen
-            plt.figure(figsize=(8, 6))
-            sns.heatmap(cm_df, annot=True, fmt='d', cmap=config_params["reports"]["cmap"], cbar = config_params["reports"]["colorbar"] ) 
-            plt.title('Confusion Matrix')
-            plt.ylabel('Actual')
-            plt.xlabel('Predicted')
-            plt.savefig("confusion_matrix.png")
-            mlflow.log_artifact("confusion_matrix.png")
-            
-            # Reporte de clasificación
-            print("Classification Report:")
-            print(classification_report(self.y_test, y_pred, target_names=config_params["reports"]["target_names"], zero_division=config_params["reports"]["clasification_report_zero_division"]))
-            report = classification_report(self.y_test, y_pred, target_names=config_params["reports"]["target_names"], zero_division=config_params["reports"]["clasification_report_zero_division"], 
-                                           output_dict=config_params["reports"]["clasification_report_output_dict"])
-            mlflow.log_metric("Accuracy",report['accuracy'])
-            mlflow.log_metric("Presicion",report['macro avg']['precision'])
-            mlflow.log_metric("Recall",report['macro avg']['recall'])
-            mlflow.log_metric("F1 score",report['macro avg']['f1-score'])
-            #Cross validation
-            scores = cross_val_score(self.model_pipeline, self.X_train, self.y_train, cv=config_params["reports"]["cv"])
-            avg_score = np.mean(scores)
-            print("Average Accuracy with CV:", avg_score)
-            mlflow.log_metric("average_accuracy_cv", avg_score)
-            mlflow.sklearn.log_model(
-                sk_model = self.model_pipeline,
-                artifact_path=config_params["mlflow_config"]["model_artifact_path"],
-                signature=signature,
-                registered_model_name=config_params["mlflow_config"]["model_name"]
-            )
-            
+        # Iniciar un experimento en MLflow
+        mlflow.log_param("grid_search_params", param_grid)
+        mlflow.log_param("cv_folds", config_params["grid_search"]["cv"])
+
+        # Entrenar el modelo con búsqueda en cuadrícula
+        grid_search.fit(self.X_train, self.y_train)
+
+        # Registrar los resultados de todas las combinaciones
+        cv_results = grid_search.cv_results_
+        for i in range(len(cv_results['params'])):
+            run_name = f"GridSearch Run {i+1}"
+            with mlflow.start_run(nested=True, run_name=run_name):
+                # Registrar los parámetros de la combinación actual
+                mlflow.log_params(cv_results['params'][i])
+                # Registrar la métrica media de validación cruzada (accuracy)
+                mlflow.log_metric(str(run_name) + " mean_test_score", cv_results['mean_test_score'][i])
+                # Registrar otras métricas opcionales si es necesario
+                mlflow.log_metric(str(run_name) + " std_test_score", cv_results['std_test_score'][i])
+
+                # Predecir con el modelo actual y registrar métricas adicionales
+                y_pred = grid_search.best_estimator_.predict(self.X_test)
+                report = classification_report(self.y_test, y_pred, target_names=config_params["reports"]["target_names"],
+                                               zero_division=config_params["reports"]["clasification_report_zero_division"],
+                                               output_dict=config_params["reports"]["clasification_report_output_dict"])
+                mlflow.log_metric(" Accuracy", report['accuracy'])
+                mlflow.log_metric(str(run_name) + " Precision", report['macro avg']['precision'])
+                mlflow.log_metric(str(run_name) + " Recall", report['macro avg']['recall'])
+                mlflow.log_metric(str(run_name) + " F1 score", report['macro avg']['f1-score'])
+
+        # Obtener el mejor modelo y sus parámetros
+        best_model = grid_search.best_estimator_
+        best_params = grid_search.best_params_
+        best_score = grid_search.best_score_
+
+        # Registrar parámetros y métricas del mejor modelo
+        mlflow.log_params(best_params)
+        mlflow.log_metric("grid_search_best_cv_score", best_score)
+
+        # Predecir con el mejor modelo
+        y_pred = best_model.predict(self.X_test)
+        signature = infer_signature(self.X_test, y_pred)
+
+        # Registrar accuracy
+        accuracy = accuracy_score(self.y_test, y_pred)
+        mlflow.log_metric("accuracy", accuracy)
+        print(f"Accuracy: {accuracy}")
+
+        print("Classification Report:")
+        report = classification_report(self.y_test, y_pred, target_names=config_params["reports"]["target_names"],
+                                       zero_division=config_params["reports"]["clasification_report_zero_division"],
+                                       output_dict=config_params["reports"]["clasification_report_output_dict"])
+        mlflow.log_metric("Classifaction_report_Accuracy",report['accuracy'])
+        mlflow.log_metric("Classifaction_report_Presicion",report['macro avg']['precision'])
+        mlflow.log_metric("Classifaction_report_Recall",report['macro avg']['recall'])
+        mlflow.log_metric("Classifaction_report_F1-score",report['macro avg']['f1-score'])
+        print(yaml.dump(report))
+
+        # Guardar el mejor modelo con MLflow
+        mlflow.sklearn.log_model(
+            sk_model=best_model,
+            artifact_path=config_params["mlflow_config"]["model_artifact_path"],
+            signature=signature,
+            registered_model_name=config_params["mlflow_config"]["model_name"]
+        )
+
 # Función principal para ejecutar el pipeline
 def main():
     mlflow.set_tracking_uri(uri=config_params["mlflow_config"]["uri"])
-    # Create a new MLflow Experiment
     mlflow.set_experiment(config_params["mlflow_config"]["experiment_name"])
-    with mlflow.start_run():
-        filepath = config_params["data"]["file_path"] 
+    with mlflow.start_run(nested = True):
+        filepath = config_params["data"]["file_path"]
         model = CervicalCancerModel(filepath)
         (model.load_data()
-            .preprocess_data()  # Aqui se eliminan outliers y se normaliza el dataset completo antes del split
-            .train_model())  # Entrenar el modelo usando el pipeline que incluye escalado y PCA
-        
+             .preprocess_data()
+             .train_model_with_grid_search())
 
 if __name__ == '__main__':
     main()
