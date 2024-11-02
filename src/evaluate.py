76126075fe98ba3ba40b78c51b11a5c9e6806890
Alejandro Emmanuel Silva Ortega <a01794545@tec.mx>
2024-11-02 12:38:39 -0600

    dvc: commit experiment 44136fa355b3678a1146ad16f7e8649e94fb4fc21fe77e8310c060f61caaff8a

--- src/evaluate.py
+++ src/evaluate.py
@@ -6,6 +6,7 @@ from sklearn.calibration import CalibratedClassifierCV
 import os
 import json
 from dvclive import Live
+#from dvclive.plots.sklearn import SKLearnPlot
 import mlflow
 import yaml
 from datetime import date
@@ -19,19 +20,23 @@ def evaluate_model(model_path, X_test_path, y_test_path, output_path, model_type
     X_test = pd.read_csv(X_test_path)
     y_test = pd.read_csv(y_test_path)
     predictions = model.predict(X_test)
+    y_score = model.predict_proba(X_test)[:,1]
+    y_score = y_score.astype(float)
     report = classification_report(y_test, predictions, zero_division=1, output_dict=True)
     cm = confusion_matrix(y_test, predictions)
+   
     
-    write_evaluation_report(output_path, report, cm, model_type)
+    write_evaluation_report(output_path, report, cm, model_type, y_test, predictions, y_score)
     predictions_pd = pd.concat([y_test,pd.DataFrame(predictions)], names=['y_true', 'y_pred'], axis = 1)
     mlflow.log_input(mlflow.data.from_pandas(X_test), context="X_test")
     mlflow.log_input(mlflow.data.from_pandas(y_test), context="y_test")
     mlflow.log_input(mlflow.data.from_pandas(pd.DataFrame(predictions)), context="predictions")
+  
     
     
     return predictions_pd
 
-def write_evaluation_report(file_path, report, confusion_matrix, model_type):
+def write_evaluation_report(file_path, report, confusion_matrix, model_type, y_true, y_pred, y_score):
     os.makedirs(os.path.dirname(file_path), exist_ok=True)
     os.makedirs(os.path.dirname(f"metrics/{model_type}_metrics.json"), exist_ok=True)
     
@@ -60,6 +65,13 @@ def write_evaluation_report(file_path, report, confusion_matrix, model_type):
         live.log_metric("precision", report['macro avg']['precision'])
         live.log_metric("recall", report['macro avg']['recall'])
         live.log_metric("F1-score", report['macro avg']['f1-score'])
+        live.log_sklearn_plot("roc", y_true, y_score)
+        #live.log_sklearn_plot("confusion_matrix", y_true, y_pred)
+        live.log_sklearn_plot("precision_recall", y_true, y_score)
+        live.log_sklearn_plot("det", y_true, y_score)
+        live.log_sklearn_plot("calibration", y_true, y_score)
+        
+        
         
   
      
